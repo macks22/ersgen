866a90baada710372dbbc9b2326cf0c3c5ab825e
Mack Sweeney <mackenzie.sweeney@gmail.com>
2015-07-29 16:21:25 -0400

    refactor PMF into fit function

--- BPMF/pmf.py
+++ BPMF/pmf.py
@@ -2,31 +2,20 @@
 PMF Implementation in Python.
 
 """
-import pandas as pd
 import numpy as np
 
 from util import predict, read_data
 
 
-if __name__ == "__main__":
-    np.random.seed(1234)
-
-    epsilon = 50    # Learning rate
-    lambda_  = 0.01  # Regularization parameter
-    momentum = 0.8
-    max_epoch = 50
-
-    header_names, train, probe = read_data()
-    uid, iid, target = header_names
+def fit_pmf(train, probe, uid='uid', iid='iid', target='target', epsilon=50,
+            lambda_=0.01, momentum=0.8, max_epoch=50, nbatches=9, N=100000,
+            nf=10):
 
     mean_rating = train[target].mean()
     ratings_test = probe[target].values.astype(np.double)
 
-    num_batches = 9
-    N = 100000  # number of training triplets processed per batch
-    n = 6040    # number of users
-    m = 3952    # number of items
-    nf = 10     # number of features (latent factor vector dimensionality)
+    n = train[uid].max() + 1  # number of users
+    m = train[iid].max() + 1  # number of items
 
     # Randomly initialize user and item feature vectors.
     w_u = 0.1 * np.random.randn(n, nf)  # User
@@ -38,12 +27,12 @@ if __name__ == "__main__":
 
     err_train = np.zeros(max_epoch)
     err_valid = np.zeros(max_epoch)
-    for epoch in range(max_epoch):
+    for epoch in xrange(max_epoch):
         rr = np.random.permutation(train.shape[0])  ## rr = random range
         train = train.ix[rr]
 
-        for batch in range(num_batches):
-            print 'epoch %d, batch %d' % (epoch + 1, batch + 1)
+        for batch in xrange(nbatches):
+            # print 'epoch %d, batch %d' % (epoch + 1, batch + 1)
 
             train_subset = train.ix[range(batch*N, (batch+1)*N)]
             uids = train_subset[uid].values
@@ -54,10 +43,10 @@ if __name__ == "__main__":
             ratings = ratings - mean_rating
 
             # Compute predictions.
-            pred_out = np.sum(w_i[iids] * w_u[uids], 1)
-            error = pred_out - ratings
-            regular = np.sum(w_i[iids] ** 2 + w_u[uids] ** 2, 1)
-            f = np.sum(error ** 2 + 0.5 * lambda_ * regular)
+            predictions = np.sum(w_i[iids] * w_u[uids], 1)
+            error = predictions - ratings
+            # regular = np.sum(w_i[iids] ** 2 + w_u[uids] ** 2, 1)
+            # loss = np.sum(error ** 2 + 0.5 * lambda_ * regular)
 
             # Compute gradients.
             IO = np.repeat(2 * error, nf).reshape(error.shape[0], nf)
@@ -79,23 +68,36 @@ if __name__ == "__main__":
             w_i -= w_i_update
 
         # Compute predictions after parameter updates.
-        pred_out = np.sum(w_i[iids] * w_u[uids], 1)
-        error = pred_out - ratings
+        predictions = np.sum(w_i[iids] * w_u[uids], 1)
+        error = predictions - ratings
         regular = np.sum(w_i[iids] ** 2 + w_u[uids] ** 2, 1)
         f_s = np.sum(error ** 2 + 0.5 * lambda_ * regular)
         err_train[epoch] = np.sqrt(f_s / N)
 
         # Compute predictions on the validation set.
         predictions = predict(w_u, w_i, mean_rating, probe)
-        error = predictions - probe[target].values
+        error = predictions - ratings_test
 
         err_valid[epoch] = np.sqrt((error ** 2).sum() / probe.shape[0])
-        print 'epoch %4d, batch %4d, Training RMSE: %6.4f, Test RMSE: %6.4f\n' % (
-            epoch, batch, err_train[epoch], err_valid[epoch])
+        print 'epoch %2d, Training RMSE: %6.4f, Test RMSE: %6.4f' % (
+            epoch, err_train[epoch], err_valid[epoch])
 
         if (epoch + 1) % 10 == 0:
             np.savetxt('w_u.csv', w_u, delimiter=',')
             np.savetxt('w_i.csv', w_i, delimiter=',')
+
+    return w_u, w_i
+
+
+if __name__ == "__main__":
+    np.random.seed(1234)
+
+    # Read data.
+    header_names, train, probe = read_data()
+    uid, iid, target = header_names
+
+    # Fit model.
+    w_u, w_i = fit_pmf(train, probe, uid, iid, target)
 
     # Finally, save weights at the end.
     np.savetxt('w_u.csv', w_u, delimiter=',')
